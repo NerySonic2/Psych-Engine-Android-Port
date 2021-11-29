63350e65e8ac5a25ea7447c09125c1f00e467844
NerySonic2 <95189360+NerySonic2@users.noreply.github.com>
2021-11-28 22:32:41 -0800

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1035,9 +1035,13 @@ class PlayState extends MusicBeatState
 				case 'senpai' | 'roses' | 'thorns':
 					if(daSong == 'roses') FlxG.sound.play(Paths.sound('ANGRY'));
 					schoolIntro(doof);
-
+					
+					case 'umm':
+				             startDialogue(dialoguejson);
+				
 				default:
 					startCountdown();
+			
 			}
 			seenCutscene = true;
 		} else {
